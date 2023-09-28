241f2db9b0968e82cf4513733c5aba6806f79625
Matthew Harding <matthewpharding@gmail.com>
2023-09-28 18:00:53 +0100

    Added an improved header to the playground

--- Opaque Types.playground/Pages/Opaque Types.xcplaygroundpage/Contents.swift
+++ Opaque Types.playground/Pages/Opaque Types.xcplaygroundpage/Contents.swift
@@ -1,6 +1,7 @@
 //: ![Swift](swift-logo.png)
-//: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/OpaqueTypes.html)
-//: # 24. Opaque Types
+//: Swift v5.7 | [Swift.org](https://docs.swift.org) | [The official Swift Language Guide](https://docs.swift.org/swift-book/documentation/the-swift-programming-language/thebasics)
+//:
+//: # Page 24. Opaque Types
 //:
 //:
 //: A function or method with an opaque return type hides its return value’s type information. Instead of providing a concrete type as the function’s return type, the return value is described in terms of the protocols it supports. Hiding type information is useful at boundaries between a module and code that calls into the module, because the underlying type of the return value can remain private. Unlike returning a value whose type is a protocol type, opaque types preserve type identity—the compiler has access to the type information, but clients of the module don’t.
